4ef386b37ad0c053fbdc94b6ec7a8dbc434a860e
LucaSoftwareEngineer <lucapc40@gmail.com>
2025-12-11 17:44:32 +0100

    implementato api che permette di registrare un nuovo pacco

---
+++ api/registra_pacco.js
@@ -0,0 +1,85 @@
+import Pacco from "../models/Pacco.js";
+
+export const registraPacco = async (req, res) => {
+  const paccoData = req.body;
+
+  if (paccoData.mittente == "" || !paccoData.mittente) {
+    res.status(400).json({ message: "il mittente è obbligatorio" });
+  }
+
+  if (paccoData.destinatario == "" || !paccoData.destinatario) {
+    res.status(400).json({ message: "il destinatario è obbligatorio" });
+  }
+
+  if (paccoData.mittenteIndirizzo == "" || !paccoData.mittenteIndirizzo) {
+    res
+      .status(400)
+      .json({ message: "l'indirizzo del mittente è obbligatorio" });
+  }
+
+  if (
+    paccoData.destinatarioIndirizzo == "" ||
+    !paccoData.destinatarioIndirizzo
+  ) {
+    res
+      .status(400)
+      .json({ message: "l'indirizzo del destinatario è obbligatorio" });
+  }
+
+  if (paccoData.prezzo == "" || !paccoData.prezzo) {
+    res.status(400).json({ message: "il prezzo è obbligatorio" });
+  }
+
+  if (isNaN(paccoData.prezzo)) {
+    res.status(400).json({ message: "il prezzo deve essere un numero valido" });
+  }
+
+  if (
+    paccoData.spedito == "" ||
+    paccoData.spedito == null ||
+    paccoData.spedito == undefined
+  ) {
+    res.status(400).json({ message: "lo stato di spedizione è obbligatorio" });
+  }
+
+  if (paccoData.consegnato == null || paccoData.consegnato == undefined) {
+    res.status(400).json({ message: "lo stato di consegna è obbligatorio" });
+  }
+
+  if (
+    paccoData.spedito &&
+    (paccoData.dataSpezione == "" || !paccoData.dataSpezione)
+  ) {
+    res.status(400).json({
+      message:
+        "la data di spedizione è obbligatoria se il pacco è stato spedito",
+    });
+  }
+
+  if (
+    paccoData.consegnato &&
+    (paccoData.dataConsegna == "" || !paccoData.dataConsegna)
+  ) {
+    res.status(400).json({
+      message:
+        "la data di consegna è obbligatoria se il pacco è stato consegnato",
+    });
+  }
+
+  try {
+    Pacco.create({
+      mittente: paccoData.mittente,
+      destinatario: paccoData.destinatario,
+      mittenteIndirizzo: paccoData.mittenteIndirizzo,
+      destinatarioIndirizzo: paccoData.destinatarioIndirizzo,
+      prezzo: paccoData.prezzo,
+      spedito: paccoData.spedito,
+      consegnato: paccoData.consegnato,
+      dataSpezione: paccoData.dataSpezione,
+      dataConsegna: paccoData.dataConsegna,
+    });
+    res.status(201).json({ message: "pacco registrato con successo" });
+  } catch (error) {
+    res.status(500).json({ message: "qualcosa è andato storto" });
+  }
+};
--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 import sequelize from "./tools/db_conn.js";
 import Pacco from "./models/Pacco.js";
+import { registraPacco } from "./api/registra_pacco.js";
+import express from "express";
 
 const main = async () => {
-    try {
-        console.log("Applicazione avviata correttamente");
-        await sequelize.sync({ force: true });
-    } catch (e) {
-        console.error("Errore durante l'avvio:", e);
-    }
+  try {
+    await sequelize.sync({ force: true });
+    console.log("Database connesso e sincronizzato con il provider orm");
+  } catch (e) {
+    console.error("Errore durante l'avvio:", e);
+  }
 };
-
 main();
+
+const app = express();
+app.use(express.json());
+
+app.post("/pacco/registra", registraPacco);
+
+app.listen(3000, () => {
+  console.log("Server in ascolto sulla porta 3000");
+});
+export default app;
